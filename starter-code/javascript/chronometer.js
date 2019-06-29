9526aef216aa30c09cdfe7769df806d6c692b41d
Olga Zinoveva <ozinka81@gmail.com>
2019-06-29 07:46:53 +0200

    first iteration done

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,39 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId = 0;
+  }
+  startClick() {
+    this.intervalId = setInterval(function() {
+      this.currentTime += 1;
+    }, 1000);
+  } // siguen en rojo 2 ultimos pintos de esta funcion en Jasmine...
+
+  getMinutes() {
+    let timeMinutes = Math.floor(this.currentTime / 60);
+    return timeMinutes;
+  }
+
+  getSeconds() {
+    let timeSeconds = 0;
+    timeSeconds = this.currentTime % 60;
+    return timeSeconds;
+  }
+
+  twoDigitsNumber(value) {
+    let resultString = value.toString();
+
+    if (resultString.length === 1) {
+      return "0" + resultString;
+    } else {
+      return resultString;
+    }
+  }
+  stopClick() {
+    this.intervalId = clearInterval(this.intervalId);
+  }
+  resetClick() {
+    this.currentTime = 0;
+  }
   // splitClick() {}
 }
